12387e83df47d3d61a79fd2dd3ee36991ed05857
Rohith C Dinesan <rohithmaddz7@gmail.com>
2023-04-22 02:17:40 +0530

    Updated project -git refresh project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing Styles 
+import './global.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Importing packages
+import {Routes, Route} from 'react-router-dom';
+import {lazy, Suspense} from 'react'; // For lazy loading components
+
+// Importing Pages
+const Home = lazy(() => import('./pages/home/home'));
+
+const App = () => {
+	return (
+		<Suspense fallback={<h1>Loading</h1>}>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<>Page Not Found</>} />
+			</Routes>
+		</Suspense>
+
+	);
 }
 
 export default App;
---
+++ frontend/src/pages/home/home.js
@@ -0,0 +1,13 @@
+// Importing Styles 
+import '../../global.css';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
